f1659f240ad41551445757e1094b9e7db90931a4
luoguoxian2017 <282513713@qq.com>
2017-12-01 22:17:58 +0800

    commit

--- index.js
+++
@@ -1,6 +0,0 @@
-const Tesseract = require('tesseract.js');
-const path = require('path');
-const myImage = path.join(__dirname,'./test.jpg');
-
-Tesseract.recognize(myImage)
-    .then(function (result) { console.log('result', result) });
