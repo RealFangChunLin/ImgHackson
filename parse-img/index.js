306bb1736a39ab91702f820720acd5f49a8a3a16
forfun <834352945@qq.com>
2017-12-02 08:57:45 +0800

    图片识别组件

---
+++ parse-img/index.js
@@ -0,0 +1,53 @@
+var fs        = require('fs');
+var tesseract = require('node-tesseract');
+var gm        = require('gm');
+
+module.exports = function imgProcess(imgPath){
+    recognizer(imgPath)
+    .then(text => {
+        console.log(`识别结果:${text}`);
+    })
+    .catch((err)=> {
+        console.error(`识别失败:${err}`);
+    });
+}
+
+/**
+ * 处理图片为阈值图片
+ * @param imgPath
+ * @param newPath
+ * @param [thresholdVal=55] 默认阈值
+ * @returns {Promise}
+ */
+function processImg (imgPath, newPath, thresholdVal) {
+    return new Promise((resolve, reject) => {
+        gm(imgPath)
+            .threshold(thresholdVal || 55)
+            .write(newPath, (err)=> {
+                if (err) return reject(err);
+
+                resolve(newPath);
+            });
+    });
+}
+
+/**
+ * 识别图片
+ * @param imgPath
+ * @param options tesseract options
+ * @returns {Promise}
+ */
+function recognizer (imgPath, options) {
+    options = Object.assign({
+        l: 'chi_sim+eng',
+        psm: 9
+    }, options);
+
+    return new Promise((resolve, reject) => {
+        tesseract
+            .process(imgPath, options, (err, text) => {
+                if (err) return reject(err);
+                resolve(text.replace(/\s/gm, ' '));
+            });
+    });
+}
